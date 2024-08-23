84f8e6301f3d534b10d4be2d69d5559238c8d995
Sana451 <sana451@mail.ru>
2024-08-23 13:56:49 +0300

    Add search form to _base.html. Add search_results to books.urls with SearchResultsListView. Add SearchResultsListView which use get_queryset Book.objects.filter(Q(title__icontains=search_query_from_form) | Q(author__icontains=search_query_from_form))

--- books/urls.py
+++ books/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from books.views import BookListView, BookDetailView
+from books.views import BookListView
+from books.views import BookDetailView
+from books.views import SearchResultsListView
 
 urlpatterns = [
     path("", BookListView.as_view(), name="book_list"),
     path("<uuid:pk>/", BookDetailView.as_view(), name="book_detail"),
+    path("search/", SearchResultsListView.as_view(), name="search_results"),
 ]
